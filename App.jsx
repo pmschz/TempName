cc380ccac15b99843bba51235e83e58d775a9089
PhamEthan <167038727+PhamEthan@users.noreply.github.com>
2025-11-05 13:51:54 -0600

    Added Light/Dark Mode and other UI improvements
    Added a light and dark mode switch to change from light or dark mode, included image in hero.

---
+++ App.jsx
@@ -0,0 +1,31 @@
+import './css/App.css'
+import Calendar from "./pages/Calendar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import {Routes, Route} from "react-router-dom"
+import NavBar from "./Components/NavBar";
+import Footer from "./Components/Footer";
+import React, { useState } from "react";
+
+
+function App() {
+  const [isDark, setDark] = useState(false);
+
+  return (
+    <div className="app-container" data-theme={isDark ? "dark" : "light"}> 
+      <NavBar isChecked={isDark} handleChange={() => setDark(!isDark)} />
+      <main className ="main">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/calendar" element={<Calendar />}/>
+          <Route path="/login" element={<Login />}/>
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
